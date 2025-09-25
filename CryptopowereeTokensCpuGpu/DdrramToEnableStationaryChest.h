233595675666525b01b5bd84b54aed5b994bc487
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-25 05:46:12 +0200

    Update DdrramToEnableStationaryChest.h

--- CryptopowereeTokensCpuGpu/DdrramToEnableStationaryChest.h
+++ CryptopowereeTokensCpuGpu/DdrramToEnableStationaryChest.h
@@ -1,1 +1,14 @@
 //we must use cintainers 
+
+
+//there is a way to corrupt iso datas.
+//but as containers in ram are flushed, we can prevent maintaining hd.
+//so once again, we need stackd. thr', this id a perfect behaviour
+//for stationnary ghedt.
+
+
+
+
+//as stacks mustnt directly communicate with gpus, and ad gpus architectue could be old,
+//we gan send signals to hd. this is corrupted.
+//to prevent it, letd use more isos. then isos must be created by gpus.
