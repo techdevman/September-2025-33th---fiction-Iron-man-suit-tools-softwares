dd96a20f9aef728f8ae1b8fdc0d19ff1aa48e051
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-25 05:48:58 +0200

    Create LungsDeleteCorruotedChestAndAddMovementRrsultsServosHeaders.hlsl

---
+++ CryptopowereeTokensCpuGpu/LungsDeleteCorruotedChestAndAddMovementRrsultsServosHeaders.hlsl
@@ -0,0 +1,4 @@
+//to prevent chedt datas corrupted, we need 
+//some simple command lines. zs this id a model output value corrections.
+//the best here is to start with energy terms. thie id also the behaviour
+//of champo2 project.
