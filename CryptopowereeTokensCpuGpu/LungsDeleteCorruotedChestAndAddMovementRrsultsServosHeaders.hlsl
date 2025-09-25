3f1ec114aa5b64594e2192ee116afb07a18c412d
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-25 06:35:25 +0200

    Update LungsDeleteCorruotedChestAndAddMovementRrsultsServosHeaders.hlsl

--- CryptopowereeTokensCpuGpu/LungsDeleteCorruotedChestAndAddMovementRrsultsServosHeaders.hlsl
+++ CryptopowereeTokensCpuGpu/LungsDeleteCorruotedChestAndAddMovementRrsultsServosHeaders.hlsl
@@ -2,3 +2,9 @@
 //some simple command lines. zs this id a model output value corrections.
 //the best here is to start with energy terms. thie id also the behaviour
 //of champo2 project.
+
+
+
+//programs running on gpu must
+//be handled by modern gpu architecture for only tokens.
+//so we can handled direct power supply here.
