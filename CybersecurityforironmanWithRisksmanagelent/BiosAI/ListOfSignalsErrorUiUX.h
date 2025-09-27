b15c64f61975bdb7b4d18ba5df4af4f548116b62
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-27 16:01:19 +0200

    Create ListOfSignalsErrorUiUX.h

---
+++ CybersecurityforironmanWithRisksmanagelent/BiosAI/ListOfSignalsErrorUiUX.h
@@ -0,0 +1,30 @@
+//the better ui ux from neiji93 was also cyber security progrzms ui ux.
+//then the errors should occur in gpus.
+//to prevent it, lets hijack cpu tonhd signzls first znd later
+//lets check old gpu architecture. so it means
+//to prevent complicated bios to gpus complex commands
+//or messages. so the conttacts must be set by gpus first.
+//after that, the boot system can still be contained in hds.
+//so thr bios can support several os. lets use it to enable light virtual machine.
+
+class loghtvirtualmachineinbiosziForCo'tract
+{
+
+public:
+  //slight machine behaviour
+  void bootosfrombiosaiWithSyncGpus();
+
+  //slight behaviour too
+    void addostobootWithonesignal();
+}
+
+
+
+//the virtual machine can provide 
+//small contracts. so the chest suit can handlen
+//messages from outside the suit. this is telecommunication.
+//it can help boot again from scratch.
+//but in order to communicate with lungs servos, we must rely on large scale containers system.
+//so we will put an os in the lungs to use models.
+//the best model here will be a chber security model.
+//sonthis is exactly this file purpose.
