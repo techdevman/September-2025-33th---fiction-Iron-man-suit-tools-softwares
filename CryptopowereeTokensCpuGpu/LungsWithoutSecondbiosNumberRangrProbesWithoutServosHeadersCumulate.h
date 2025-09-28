59da5bb74a8834038f98602aced822fa5b66185c
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-28 15:34:57 +0200

    Create LungsWithoutSecondbiosNumberRangrProbesWithoutServosHeadersCumulate.h

---
+++ CryptopowereeTokensCpuGpu/LungsWithoutSecondbiosNumberRangrProbesWithoutServosHeadersCumulate.h
@@ -0,0 +1,11 @@
+//if we dont deal with chest second bios s, we can
+//prevent 2 cpu messages. then contracts are the keys with tokens.
+//as every containers in a same manner. so as we fullfill crypto
+//requirements, we can easily use linux scheduller.
+//after that, the cpu processes mustnt use async keyword.
+//then, as we fullfill our stuff with cyber decurity neiji93 in github, we can use
+//at least 20 000 probed in lungs (linux max processes). plus,
+//the engeneering process tells us that this is the same
+//without tokens. so we can use at least 40 000 probes. but here
+//the best choice would be to use 2500 probes like in chest beczuse we
+//have to trst our programs (cyber decurity, subsystems). plus, we cannot prevent high speed schedulling.
